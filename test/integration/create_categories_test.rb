5ce84221ac443515250e27a3324ff1079d40067c
Daniel Zanfir <danielzanfir98@gmail.com>
2020-10-28 17:28:24 +0200

    modified create_categories_test.rb tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,31 +3,29 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+    @admin_user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+    sign_in_as(@admin_user, "password")
   end
 
   test "get new category form and create category" do
-    sign_in_as(@user, "password")
-    get new_category_path
-
-    assert_template 'categories/new'
+    get "/categories/new"
+    assert_response :success
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: "FIFA" } }
-      follow_redirect!
+      post categories_path, params: { category: { name: "FireWatch"} }
+      assert_response :redirect
     end
-    assert_template 'categories/index'
-    assert_match "FIFA", response.body
+    follow_redirect!
+    assert_response :success
+    assert_match "FireWatch", response.body
   end
 
-  test "invalid category submission results in failure" do
-    sign_in_as(@user, "password")
-    get new_category_path
-
-    assert_template 'categories/new'
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: " " } }
+      post categories_path, params: { category: { name: " "} }
     end
-    assert_template 'categories/new'
+    assert_match "errors", response.body
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
